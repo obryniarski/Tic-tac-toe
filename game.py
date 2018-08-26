a1c1518f1bfec0e5785636b08d0844113736f339
Oliver <obryniarski@berkeley.com>
2018-08-25 23:05:04 -0700

    added board constructor and selectors

--- game.py
+++ game.py
@@ -9,3 +9,26 @@ def factorial(n):
 
 
 print(factorial(5))
+
+
+def new_board():
+    placeholder = '~'
+    row = [placeholder, placeholder, placeholder]
+    return [row, row, row]
+
+
+def position(board, row, column):
+    return board[row][column]
+
+
+def print_board(b):
+    for i in range(3):
+        row = ''
+        for j in range(3):
+            row += b[i][j] + ' '
+        row.strip(' ')
+        print(row)
+
+
+test_board = new_board()
+print_board(test_board)
