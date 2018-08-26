adcb9feef7b26785a8945b7f3fd9e3c8d4ffe028
Oliver <obryniarski@berkeley.com>
2018-08-26 01:36:02 -0700

    removed random stuff from git practice earlier

--- game.py
+++ game.py
@@ -1,16 +1,3 @@
-print('hello world!')
-
-
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-print(factorial(5))
-
-
 def new_board():
     return [['~', '~', '~'], ['~', '~', '~'], ['~', '~', '~']]
 
